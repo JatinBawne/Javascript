a0c1b95cc8445037b5ab04d1e6c4df28efa9c351
JatinBawne <jatinbawne2347@gmail.com>
2024-07-24 23:15:45 +0530

    DataTypes added

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS cod as newer version
+
+// alert("Hello") //we are using nodeJS, not browser
+
+console.log(3 + 3)
+console.log("Hitesh")
+
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = false
+
+
+//                     <----- PRIMITIVE DATATYPES ----->
+// number => 2^53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone type
+// undefined => 
+// symbol => uniqueness
+
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
