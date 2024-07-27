76eeeb570f933c45ca2311f839beae9a4408b8d5
JatinBawne <jatinbawne2347@gmail.com>
2024-07-27 16:16:11 +0530

    03 conversion of Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+
+
+// let score = undefined
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+/*
+    "33" = 33
+    "abc" = NaN - Not a Number
+    null = 0
+    true = 1
+    false = 0
+    undefined = NaN
+*/
+
+// let isLoggedIn = 45
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+
+let someNumber = 34
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
