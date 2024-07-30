397152645c935891bf88dc0edf4ce269f822462f
JatinBawne <jatinbawne2347@gmail.com>
2024-07-30 13:30:12 +0530

    nums and maths added

---
+++ 01_basics/07_nums_ans_maths.js
@@ -0,0 +1,42 @@
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(100)
+// console.log(balance)
+// console.log(typeof balance)
+
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 1223.8966
+// console.log(otherNumber.toPrecision(4))
+// console.log(otherNumber.toPrecision(3)) //gives exponential
+
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-in'))
+
+// //Maximums and Minimums
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Number.MIN_SAFE_INTEGER)
+
+
+
+
+// ************* MATHS *****************
+// console.log(Math)
+// console.log(Math.abs(-24))
+// console.log(Math.round(4.4))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.2))
+// console.log(Math.min(5, 2, 6, 12))
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log((Math.floor(Math.random() * (max - min + 1)) + min))
